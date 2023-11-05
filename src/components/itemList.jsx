fbdeeb4eb8747f9a2c600ff6cd7d7570abda4914
danoGuerrero <danielguerrerodev@gmail.com>
2023-11-05 12:54:43 -0600

    redux functionality

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -1,13 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators } from "../store/actions";
+
 function ItemList() {
-  return (
-    <>
-      <div className="item-container">
-        <input type="checkbox" name="a" id="a" value="a" />
-        <label htmlFor="a">hola soy un checkbox</label>
-        <button>X</button>       
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.items);
+
+  const addItemToBasket = (index) => {
+    dispatch(actionCreators.addToBasket(index));
+  };
+
+  const removeItemFromBasket = (index) => {
+    dispatch(actionCreators.removeItem(index));
+  };
+
+  const showItems = items.map((item, index) => {
+    return (
+      <div
+        className={`item-container ${item.inBasket && `highlight`}`}
+        key={index}
+      >
+        <input
+          type="checkbox"
+          checked={item.inBasket}
+          onChange={() => addItemToBasket(index)}
+          name={item.value}
+          id={item.value + index}
+          value={item.value}
+        />
+        <label htmlFor={item.value}>{item.value}</label>
+        <button onClick={() => removeItemFromBasket(index)}>X</button>
       </div>
-    </>
-  );
+    );
+  });
+
+  return <>{showItems}</>;
 }
 
 export default ItemList;
